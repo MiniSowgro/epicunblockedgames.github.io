e29890e511cad9a8ab4e3ec9d6633957b3264ad6
MiniSowgro <132415164+MiniSowgro@users.noreply.github.com>
2024-05-01 21:32:19 +0800

    Update javascript.js
    added hakari (i hope)

--- javascript.js
+++ javascript.js
@@ -4,4 +4,8 @@ function clicked() {
 window.alert("No games yet!")
 }
 
+var img = document.createElement("img")
+img.src = "https://staticg.sportskeeda.com/editor/2024/01/439e4-17042428312455-1920.jpg"
+document.body.appendChild(img)
+
 button.addEventListener('click', clicked)
